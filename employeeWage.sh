88f91199ca28c336c7dfd6b32f952b53ad6345cd
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-28 15:11:37 +0530

    ADDED: attendace feature

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to employee wage computation program"
+
+isPresent=$(($RANDOM%2))
+
+if [ $isPresent -eq 1 ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
